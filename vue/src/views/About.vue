3c847a945d0f82707b43c1526507ca5cb109f783
Gabrieltht <480117884@qq.com>
2023-09-04 00:33:33 -0400

    upload frontend

---
+++ vue/src/views/About.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "About"
+})
+</script>
+
+<template>
+  <div>
+      This is about page.
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/Contact.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "Contact"
+})
+</script>
+
+<template>
+  <div>
+      This is contact page.
+  </div>
+</template>
+
+<style scoped>
+
+</style>
